65eed6d25851ae6690a97dce15fa7bb755842716
Dewayne Allen <dewayneallen76@gmail.com>
2017-04-14 14:54:43 -0500

    no changes

--- InheritancePractice/app/src/main/java/com/example/android/inheritancepractice/MainActivity.java
+++ InheritancePractice/app/src/main/java/com/example/android/inheritancepractice/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-    
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
