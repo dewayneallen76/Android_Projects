6350af2670ec1b582bafe706a9acdea8d8f3e256
Dewayne Allen <dewayneallen76@gmail.com>
2017-04-16 22:52:52 -0500

    made the quantity 1, since that is the minimum that can be ordered. added an intent to send and email when the orderSummary method is called by clicking the order button

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -6,6 +6,8 @@
  */
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -45,6 +47,14 @@ public class MainActivity extends AppCompatActivity {
 
         int price = calculatePrice(hasWhippedCream, hasChocolateTopping);
         String priceMessage = createOrderSummary(name, price, hasWhippedCream, hasChocolateTopping);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for " + name);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
         displayMessage(priceMessage);
     }
 
