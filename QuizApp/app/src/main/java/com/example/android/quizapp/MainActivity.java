14500466bc9811119d9d2a38441ad2fb6eeb46c4
Dewayne Allen <dewayneallen76@gmail.com>
2017-04-19 10:13:01 -0500

    starting to working on backend functionality. set variable for score, added submitQuiz method, beginning with get name from the name field

--- QuizApp/app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ QuizApp/app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.android.quizapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submitQuiz(View view) {
+        // Gets the name entered in the edit text view
+        EditText nameField = (EditText) findViewById(R.id.name_field);
+        String name = nameField.getText().toString();
     }
 }
