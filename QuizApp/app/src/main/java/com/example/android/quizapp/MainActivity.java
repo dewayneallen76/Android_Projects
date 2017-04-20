0c0a6c5d68e463b9163f01c65818d523212092d4
Dewayne Allen <dewayneallen76@gmail.com>
2017-04-20 14:15:06 -0500

    added text id to activity xml, added method to hopefully display a quiz summary message

--- QuizApp/app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ QuizApp/app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         CheckBox checkbox2Question1 = (CheckBox) findViewById(R.id.checkbox2_question1);
         boolean hasCheckbox2Question1 = checkbox2Question1.isChecked();
 
+        //Checks to see if the correct answer for questions with radio buttons is selected
         RadioButton radioButtonQuestion2 = (RadioButton) findViewById(R.id.radio_button_1);
         boolean hasRadioButtonQuestion2 = radioButtonQuestion2.isChecked();
 
@@ -46,7 +47,11 @@ public class MainActivity extends AppCompatActivity {
 
         calculateScore(hasCheckbox1Question1, hasCheckbox2Question1, hasRadioButtonQuestion2,
                         hasRadioButtonQuestion3, hasRadioButtonQuestion4);
-        displayScore(score);
+
+        String quizMessage = createQuizSummary(name, score, hasCheckbox1Question1,hasCheckbox2Question1, hasRadioButtonQuestion2,
+         hasRadioButtonQuestion3, hasRadioButtonQuestion4, bestPlayer);
+
+        displayMessage(quizMessage);
     }
 
     /**
@@ -56,7 +61,8 @@ public class MainActivity extends AppCompatActivity {
      * @param hasCheckbox2Question1 is whether the user has selected the correct answer checkbox
      * @return total score
      */
-    public int calculateScore(boolean hasCheckbox1Question1, boolean hasCheckbox2Question1, boolean hasRadioButtonQuestion2, boolean hasRadioButtonQuestion3, boolean hasRadioButtonQuestion4) {
+    public int calculateScore(boolean hasCheckbox1Question1, boolean hasCheckbox2Question1, boolean hasRadioButtonQuestion2,
+                              boolean hasRadioButtonQuestion3, boolean hasRadioButtonQuestion4) {
         // Check to see if the correct answers are selected for question 1
         if(hasCheckbox1Question1 && hasCheckbox2Question1) {
             score = score + 5;
@@ -76,16 +82,47 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the total score on the screen.
      */
-    public void displayScore(int totalScore) {
+    public void displayScore(String createQuizSummary) {
         TextView scoreTextView = (TextView) findViewById(R.id.score_text_view);
-        scoreTextView.setText("" + totalScore);
+        scoreTextView.setText(createQuizSummary);
     }
 
     /**
      * This method resets the score to zero when clicking the reset button.
      */
-    public void resetQuiz(View v) {
-        score = 0;
-        displayScore(score);
+//    public void resetQuiz(View v) {
+//        displayScore(score);
+//    }
+
+    public void displayMessage(String message) {
+        TextView quizSummaryTextView  = (TextView) findViewById(R.id.quiz_summary_text_view);
+        quizSummaryTextView.setText(message);
+    }
+
+    public String createQuizSummary(String name, int score, boolean hasCheckbox1Question1, boolean hasCheckbox2Question1, boolean hasRadioButtonQuestion2,
+                                      boolean hasRadioButtonQuestion3, boolean hasRadioButtonQuestion4, String bestPlayer) {
+        String quizMessage = "Name: " + name;
+        //Will display message
+        quizMessage += "\nYou scored " + score + "points!";
+        if(hasCheckbox1Question1 == true && hasCheckbox2Question1) {
+            quizMessage += "\nTony Parker and Manu Ginobili were the two players to win four championships with Tim Duncan.";
+        }
+        //Will display message
+        if(hasRadioButtonQuestion2 == true) {
+            quizMessage += "\nGregg Poppovich took over coaching the Spurs in 1996";
+        }
+        //
+        if(hasRadioButtonQuestion3 == true) {
+            quizMessage += "\nDavid Robinson played college basketball at the Naval Aacademy.";
+        }
+        //
+        if(hasRadioButtonQuestion4 == true) {
+            quizMessage += "\nThe Spurs coyote was the first mascot ejected from a game in 2005";
+
+        }
+        quizMessage += "\nYou chose " + bestPlayer + " as the best Spurs player ever. Good choice!";
+        quizMessage += "\nThanks for taking the Spurs quiz";
+
+        return quizMessage;
     }
 }
