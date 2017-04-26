c15ede2d197407fff30f381fcd6bfaa93cda73bb
Dewayne Allen <dewayneallen76@gmail.com>
2017-04-25 23:23:58 -0500

    added while loop to display each word in the words array list

--- Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,8 @@ package com.example.android.miwok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -11,7 +13,7 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
-        
+
         // Array list with strings for numbers (english translation)
         ArrayList<String> words = new ArrayList<String>();
         words.add("one");
@@ -24,5 +26,22 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("eight");
         words.add("nine");
         words.add("ten");
+
+        // Find the root view so that we can add child views to it
+        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+        // Initialize variable to keep track of the current index position
+        int index = 0;
+        // Keep looping until we've reached the end of the list (keep looping as long
+        // as the current index position is less than the length of the list)
+        while (index < words.size()) {
+            // Create a new text view
+            TextView wordView = new TextView(this);
+            // Set the text to be the word at the index
+            wordView.setText(words.get(index));
+            // Add this text as a child view to the root view
+            rootView.addView(wordView);
+            // Increment the index value by 1
+            index++;
+        }
     }
 }
