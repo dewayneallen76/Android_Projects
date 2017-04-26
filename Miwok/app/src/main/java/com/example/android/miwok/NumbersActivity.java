6c1673db7d1d77f87f2c21a66331376858fe1f5c
Dewayne Allen <dewayneallen76@gmail.com>
2017-04-25 22:53:08 -0500

    created an array list words containing string values for english translation for each word

--- Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,8 +1,9 @@
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,20 +11,18 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
-
-        // Array of words
-        String [] words = new String [10];
-        words [0] = "one";
-        words [1] = "two";
-        words [2] = "three";
-        words [3] = "four";
-        words [4] = "five";
-        words [5] = "six";
-        words [6] = "seven";
-        words [7] = "eight";
-        words [8] = "nine";
-        words [9] = "ten";
-
-        Log.v("NumbersActivity", "Word at index 0: " + words[0]);
+        
+        // Array list with strings for numbers (english translation)
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
     }
 }
