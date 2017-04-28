53d8a521090e533ff741d00d98de867d8e2df65a
Dewayne Allen <dewayneallen76@gmail.com>
2017-04-27 22:03:16 -0500

    updated word adapter to get the image and display the image correctly updated the list_item.xml imageview with id list_item_icon

--- Miwok/app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ Miwok/app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -55,6 +56,12 @@ public class WordAdapter extends ArrayAdapter<Word> {
         // Get the default translation from the currentWord object and
         // set this text on the default TextView
         defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        // Find the ImageView in the list_item.xml layout with the ID list_item_icon
+        ImageView iconView = (ImageView) listItemView.findViewById(R.id.list_item_icon);
+        // Get the image resource ID from the current Word object and
+        // set the image to iconView
+        iconView.setImageResource(currentWord.getImageResourceID());
 
         // Return the whole list item layout (containing 2 TextViews)
         // so that it can be shown in the ListView
