fa87f4c28b2f26a345a929aa6113dd998b917e00
Dewayne Allen <dewayneallen76@gmail.com>
2017-05-02 16:24:11 -0500

    added helper code so that information can be displayed to check db connection

---
+++ Pets App/app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -0,0 +1,10 @@
+package com.example.android.pets.data;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by dewayneallen on 5/2/17.
+ */
+
+public class PetDbHelper extends SQLiteOpenHelper {
+}
