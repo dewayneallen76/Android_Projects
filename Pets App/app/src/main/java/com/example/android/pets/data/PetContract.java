9b71e6268efb3ea383689b471bc9ea0c89aa1a22
Dewayne Allen <dewayneallen76@gmail.com>
2017-05-04 15:25:55 -0500

    added method to pet entry to validate gender

--- Pets App/app/src/main/java/com/example/android/pets/data/PetContract.java
+++ Pets App/app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -82,5 +82,16 @@ public final class PetContract {
         public static final int GENDER_MALE = 1;
         public static final int GENDER_FEMALE = 2;
 
+        /**
+         * Returns whether or not the given gender is {@link #GENDER_FEMALE} {@link #GENDER_MALE} or
+         * {@link #GENDER_UNKNOWN}
+         */
+        public static boolean isValidGender(int gender) {
+            if(gender == GENDER_FEMALE || gender == GENDER_MALE || gender == GENDER_UNKNOWN) {
+                return true;
+            }
+            return false;
+        }
+
     }
 }
