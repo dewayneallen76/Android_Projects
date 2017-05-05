927ff8948310602be12ed3ce6cded70183366a7f
Dewayne Allen <dewayneallen76@gmail.com>
2017-05-05 11:40:31 -0500

    added new java class PetCursorAdapter to setup cursor adapter to get data from the databse for views

---
+++ Pets App/app/src/main/java/com/example/android/pets/data/PetCursorAdapter.java
@@ -0,0 +1,11 @@
+package com.example.android.pets.data;
+
+import android.widget.CursorAdapter;
+
+/**
+ * Created by dewayneallen on 5/5/17.
+ */
+
+public PetCursorAdapter extends CursorAdapter {
+            
+}
