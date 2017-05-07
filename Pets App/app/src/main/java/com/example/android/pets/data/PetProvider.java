e61f3bf5895d3e31fca588330773ebf4ff69e775
Dewayne Allen <dewayneallen76@gmail.com>
2017-05-07 16:35:55 -0500

    added notify change so that the database will only autmatically reload if status of the database changes, and not everytime

--- Pets App/app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ Pets App/app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -106,6 +106,13 @@ public class PetProvider extends ContentProvider {
             default:
                 throw new IllegalArgumentException("Cannot query unknown URI " + uri);
         }
+
+        // Set notification URI on the Cursor, so we know what content URI the Cursor was created
+        // for.
+        // If the data at this URI changes, then we know we need to update the cursor.
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+        // Return the cursor.
         return cursor;
     }
 
@@ -158,6 +165,10 @@ public class PetProvider extends ContentProvider {
             Log.e(LOG_TAG, "Failed to insert for row " + uri);
             return null;
         }
+
+        // Notify all listeners that the data has changed for the pet content URI.
+        // uri:content://com.example.android.pets/pets
+        getContext().getContentResolver().notifyChange(uri, null);
 
         // Once we know the ID of the new row in the table,
         // return the new URI with the ID appended to the end of it
@@ -234,9 +245,18 @@ public class PetProvider extends ContentProvider {
         // Otherwise, get writable database
         SQLiteDatabase database = mDbHelper.getWritableDatabase();
 
-        // Returns the number of database rows affected by the update statement.
-        return database.update(PetContract.PetEntry.TABLE_NAME, values, selection, selectionArgs);
+        // Perform the update on the database and get the number of rows affected.
+        int rowsUpdated = database.update(PetContract.PetEntry.TABLE_NAME, values, selection, selectionArgs);
 
+        // If 1 or more of the rows were updated, then notify all listeners that the data at the
+        // given URI has changed.
+
+        if (rowsUpdated != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        // Return the number of rows updated.
+        return rowsUpdated;
     }
 
     /**
@@ -247,19 +267,32 @@ public class PetProvider extends ContentProvider {
         // Get writeable database
         SQLiteDatabase database = mDbHelper.getWritableDatabase();
 
+        // Track the number of rows that were deleted
+        int rowsDeleted;
+
         final int match = sUriMatcher.match(uri);
         switch (match) {
             case PETS:
                 // Delete all rows that match the selection and selection args
-                return database.delete(PetContract.PetEntry.TABLE_NAME, selection, selectionArgs);
+                rowsDeleted = database.delete(PetContract.PetEntry.TABLE_NAME, selection, selectionArgs);
+                break;
             case PET_ID:
                 // Delete a single row given by the ID in the URI
                 selection = PetContract.PetEntry._ID + "=?";
                 selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
-                return database.delete(PetContract.PetEntry.TABLE_NAME, selection, selectionArgs);
+                rowsDeleted=  database.delete(PetContract.PetEntry.TABLE_NAME, selection, selectionArgs);
+                break;
             default:
                 throw new IllegalArgumentException("Deletion is not supported for " + uri);
         }
+
+        // If 1 or more rows deleted, then notify all listeners that the data at the given URI
+        // has changed.
+        if (rowsDeleted != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        // Return the number of rows deleted.
+        return rowsDeleted;
     }
 
     /**
