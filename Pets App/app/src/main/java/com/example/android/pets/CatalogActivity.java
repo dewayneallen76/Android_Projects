8de34244c1bcb229b29461b828ec428a42a6ad27
Dewayne Allen <dewayneallen76@gmail.com>
2017-05-08 14:15:08 -0500

    added helper method for deleting all pets from the database.

--- Pets App/app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ Pets App/app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -26,6 +26,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -128,6 +129,14 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
 
     }
 
+    /**
+     * Helper method to delete all pets.
+     */
+    public void deleteAllPets() {
+        int rowsDeleted = getContentResolver().delete(PetContract.PetEntry.CONTENT_URI, null, null);
+        Log.v("Catalog Activity", rowsDeleted + " rows deleted from database");
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu options from the res/menu/menu_catalog.xml file.
@@ -146,7 +155,7 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
                 return true;
             // Respond to a click on the "Delete all entries" menu option
             case R.id.action_delete_all_entries:
-                // Do nothing for now
+                deleteAllPets();
                 return true;
         }
         return super.onOptionsItemSelected(item);
