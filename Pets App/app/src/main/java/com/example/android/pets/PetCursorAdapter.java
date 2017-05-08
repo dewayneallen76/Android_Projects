f4bb2875c89e80adb976f7c4f8308f9c2e5daa93
Dewayne Allen <dewayneallen76@gmail.com>
2017-05-08 14:23:49 -0500

    updated bindView method so that if the breed is empty or null it will populate unknown breed in the text view

--- Pets App/app/src/main/java/com/example/android/pets/PetCursorAdapter.java
+++ Pets App/app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -2,6 +2,7 @@ package com.example.android.pets;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -64,6 +65,12 @@ public class PetCursorAdapter extends CursorAdapter {
         // Read the pet attributes from the Cursor for the current pet
         String petName = cursor.getString(nameColumnIndex);
         String petBreed = cursor.getString(breedColumnIndex);
+
+        // If pet breed is empty string or null, then use default text that says unknown breed,
+        // so the TextView isn't blank.
+        if (TextUtils.isEmpty(petBreed)) {
+            petBreed = context.getString(R.string.unknown_breed);
+        }
 
         // Populate fields with extracted properties
         nameTextView.setText(petName);
