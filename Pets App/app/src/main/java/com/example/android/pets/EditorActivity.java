81fd377793c049a4770b54de31a8cf9a5fb68497
Dewayne Allen <dewayneallen76@gmail.com>
2017-05-08 11:40:43 -0500

    setup the savePet method to make sure that there are values in the edit entries

--- Pets App/app/src/main/java/com/example/android/pets/EditorActivity.java
+++ Pets App/app/src/main/java/com/example/android/pets/EditorActivity.java
@@ -147,14 +147,29 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
         String nameString = mNameEditText.getText().toString().trim();
         String breedString = mBreedEditText.getText().toString().trim();
         String weightString = mWeightEditText.getText().toString().trim();
-        int weight = Integer.parseInt(weightString);
 
+        // Check if this is supposed to be a new pet
+        // and check if all the fields in the editor are blank.
+        if (mCurrentPetUri == null &&
+                TextUtils.isEmpty(nameString) && TextUtils.isEmpty(breedString) &&
+                TextUtils.isEmpty(weightString) && mGender == PetContract.PetEntry.GENDER_UNKNOWN) {
+                // Since no fields were modified, we can return early without creating a new pet.
+                // No need to create ContentValues and no need to do any ContentProvider operations.
+            return;
+        }
         // Create a content values object where column names are the keys,
         // and pet attributes from the editor are the values.
         ContentValues values = new ContentValues();
         values.put(PetContract.PetEntry.COLUMN_PET_NAME, nameString);
         values.put(PetContract.PetEntry.COLUMN_PET_BREED, breedString);
         values.put(PetContract.PetEntry.COLUMN_PET_GENDER, mGender);
+
+        // If the weight is not provided by the user, don't try to parse the string into and integer
+        // value. Use 0 by default.
+        int weight = 0;
+        if (!TextUtils.isEmpty(weightString)) {
+            weight = Integer.parseInt(weightString);
+        }
         values.put(PetContract.PetEntry.COLUMN_PET_WEIGHT, weight);
 
         // Determine if this is a new or existing pet by checking if mCurrentPetUri is null or not.
